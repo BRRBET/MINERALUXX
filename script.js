f0c98a23419c259756c8ec80b0f2a4666250c3d3
BRRBET <cafeconpanxd053@gmail.com>
2024-11-27 17:39:35 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("login-btn").addEventListener("click", function () {
-  window.location.href = "dashboard.html"; // Cambia "dashboard.html" por la URL a donde se redirige después del inicio de sesión
-});
-
-document.getElementById("register-btn").addEventListener("click", function () {
-  window.location.href = "Registro.html"; // Cambia "register.html" por la URL de la página de registro
-});
