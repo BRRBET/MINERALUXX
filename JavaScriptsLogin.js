c4bb7969a745824b9d77f91c3ab75c506fc39a21
BRRBET <cafeconpanxd053@gmail.com>
2024-11-27 17:59:04 -0500

    Create JavaScriptsLogin.js

---
+++ JavaScriptsLogin.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const loginButton = document.getElementById("login");
+  const registerButton = document.getElementById("register");
+
+  // Redirigir al dashboard
+  loginButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    alert("Inicio de sesión exitoso!");
+    window.location.href = "dashboard.html";
+  });
+
+  // Redirigir a registro
+  registerButton.addEventListener("click", () => {
+    window.location.href = "registro.html";
+  });
+});
