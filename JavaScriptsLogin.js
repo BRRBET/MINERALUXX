8ce297bd93a9f7e825967fdc476b221777adf2a3
BRRBET <cafeconpanxd053@gmail.com>
2024-11-27 17:44:35 -0500

    Update JavaScriptsLogin.js

--- JavaScriptsLogin.js
+++ JavaScriptsLogin.js
@@ -1,1 +1,7 @@
+document.getElementById("login-btn").addEventListener("click", function () {
+  window.location.href = "dashboard.html"; // Cambia "dashboard.html" por la URL a donde se redirige después del inicio de sesión
+});
 
+document.getElementById("register-btn").addEventListener("click", function () {
+  window.location.href = "register.html"; // Cambia "register.html" por la URL de la página de registro
+});
