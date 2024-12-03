323b7c92a3bcaa1d2feb0a2e81700ca76428132a
BRRBET <cafeconpanxd053@gmail.com>
2024-12-03 14:26:33 -0500

    Update JavaScriptsHome.js

--- JavaScriptsHome.js
+++ JavaScriptsHome.js
@@ -1,3 +1,18 @@
+// Función para mostrar el alert con la información de cómo funciona
+function showAlert() {
+  const infoMessage = `
+    ¡Bienvenido a nuestra plataforma de robots!
+    En nuestra página puedes invertir en robots para obtener ganancias pasivas.
+    Cada robot tiene un precio de 15 USDT y genera 3 USDT diariamente.
+    Además, al referir amigos podrás ganar comisiones adicionales.
+    
+    ¡Únete ahora y empieza a ganar!
+  `;
+
+  // Muestra la alerta con el mensaje de cómo funciona
+  alert(infoMessage);
+}
+
 // Redirigir a otra página
 function redirectTo(url) {
   window.location.href = url;
