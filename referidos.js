69284df7caad533e2efac7e4c973c278f5ef31af
GITMINING <cafeconpanxd053@gmail.com>
2024-12-16 22:31:51 -0500

    Update referidos.js

--- referidos.js
+++ referidos.js
@@ -17,7 +17,7 @@ function generateReferralCode() {
 // Función para mostrar el enlace de referido y el código en su lugar correspondiente
 function displayReferralLink() {
   const referralCode = generateReferralCode();
-  const referralLink = `https://brrbet.github.io/MINERALUXPLUX/Registro.html`;
+  const referralLink = `https://brrbet.github.io/GITMINING/Registro.html`;
 
   const referralCodeElement = document.getElementById("random-id");
   if (referralCodeElement) {
